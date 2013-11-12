490af97f5f7fa8e3a33c9cd46984e5775c284ae0
shivanyshenoy <shivanykshenoy@yahoo.com>
2013-11-12 00:35:20 -0600

    enabled JSLint and fixed errors

--- angularjs/fibonnaci/FibonacciCtrl.js
+++ angularjs/fibonnaci/FibonacciCtrl.js
@@ -10,16 +10,16 @@
         $scope.result = 0;
 
         $scope.$watch('fibonacciInput', function () {
-            var num = parseInt($scope.fibonacciInput), i, result = 0, result_1 = 0, result_2 = 0;
+            var num = parseInt($scope.fibonacciInput, 10), i, result = 0, result_1 = 0, result_2 = 0;
 
-            if (num == 1) {
+            if (num === 1) {
                 result = 1;
-            } else if (num == 2) {
+            } else if (num === 2) {
                 result = 2;
             } else {
                 result_1 = 1;
                 result_2 = 1;
-                for(i=2; i<=num; i=i+1) {
+                for (i = 2; i <= num; i = i + 1) {
                     result = result_1 + result_2;
                     result_2 = result_1;
                     result_1 = result;
