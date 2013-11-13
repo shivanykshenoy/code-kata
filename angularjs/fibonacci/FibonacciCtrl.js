90dc671e74f62da89b6033e3073712b430fe1bd6
shivanyshenoy <shivanykshenoy@yahoo.com>
2013-11-12 19:29:07 -0600

    factored in edge cases.

--- angularjs/fibonacci/FibonacciCtrl.js
+++ angularjs/fibonacci/FibonacciCtrl.js
@@ -12,21 +12,29 @@
         $scope.$watch('fibonacciInput', function () {
             var num = parseInt($scope.fibonacciInput, 10), i, result = 0, result_1 = 0, result_2 = 0;
 
-            if (num === 1) {
-                result = 1;
-            } else if (num === 2) {
-                result = 2;
+            if (num < 0 || Number.isNaN(num)) {
+                result = "Invalid entry";
+            } else if (num <= 2) {
+                result = num;
             } else {
                 result_1 = 1;
                 result_2 = 1;
                 for (i = 2; i <= num; i = i + 1) {
-                    result = result_1 + result_2;
-                    result_2 = result_1;
-                    result_1 = result;
+
+                    if (result <= Number.MAX_VALUE) {
+                        result = result_1 + result_2;
+                        result_2 = result_1;
+                        result_1 = result;
+                    } else {
+                        //if the result hits the max value for a number
+                        //record it as Not a number;
+                        result = "Number too large";
+                    }
+
                 }
+
             }
             $scope.result = result;
         });
-
     }]);
 }());
--- test/fibonacci/fibonacciSpec.js
+++ test/fibonacci/fibonacciSpec.js
@@ -19,39 +19,57 @@
         });
 
         it("f(0) should return 0", function () {
-            scope.fibonacciInput =0;
+            scope.fibonacciInput = 0;
             scope.$apply();
             expect(scope.result).toEqual(0);
         });
 
         it("f(1) should return 1", function () {
-            scope.fibonacciInput =1;
+            scope.fibonacciInput = 1;
             scope.$apply();
             expect(1).toEqual(scope.result);
         });
 
         it("f(2) should return 3", function () {
-            scope.fibonacciInput =2;
+            scope.fibonacciInput = 2;
             scope.$apply();
             expect(2).toEqual(scope.result);
         });
 
         it("f(3) should return 3", function () {
-            scope.fibonacciInput =3;
+            scope.fibonacciInput = 3;
             scope.$apply();
             expect(3).toEqual(scope.result);
         });
 
         it("f(4) should return 5", function () {
-            scope.fibonacciInput =4;
+            scope.fibonacciInput = 4;
             scope.$apply();
             expect(5).toEqual(scope.result);
         });
 
         it("f(5) should return 8", function () {
-            scope.fibonacciInput =5;
+            scope.fibonacciInput = 5;
             scope.$apply();
             expect(8).toEqual(scope.result);
+        });
+
+        it("f(3234) should return Number too large", function () {
+            scope.fibonacciInput = 3234;
+            scope.$apply();
+            expect("Number too large").toEqual(scope.result);
+        });
+
+        it("f(-1) should return Invalid entry", function () {
+            scope.fibonacciInput = -1;
+            scope.$apply();
+            expect("Invalid entry").toEqual(scope.result);
+        });
+
+        it("f(test) should return Invalid entry", function () {
+            scope.fibonacciInput = "test";
+            scope.$apply();
+            expect("Invalid entry").toEqual(scope.result);
         });
 
     });
